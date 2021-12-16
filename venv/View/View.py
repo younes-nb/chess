9177ec931b373e8c9e5104e956968fcbfc58dadd
younes <younesnikbin@gmail.com>
2021-12-16 20:28:27 +0330

    add menu bar

--- venv/View/View.py
+++ venv/View/View.py
@@ -1,9 +1,38 @@
-from PyQt6.QtWidgets import QWidget
+import os
+from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QMenuBar, QMenu
+from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtCore import Qt
 
 
-class View(QWidget):
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+class View(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.move(100, 100)
-        self.setFixedSize(800, 600)
+        self.setWindowState(Qt.WindowState.WindowMaximized)
         self.setWindowTitle("Chess")
+        self.setWindowIcon(QIcon(resource_path("Images/icon.png")))
+        menuBar = self.menuBar()
+        options = menuBar.addMenu("Options")
+
+        self.undo = QAction(QIcon(resource_path("Images/undo.png")), "Undo")
+        self.undo.setShortcut("Ctrl+Z")
+        options.addAction(self.undo)
+
+        self.redo = QAction(QIcon(resource_path("Images/redo.png")), "Redo")
+        self.redo.setShortcut("Ctrl+Y")
+        options.addAction(self.redo)
+
+        self.reset = QAction(QIcon(resource_path("Images/reset.png")), "Reset")
+        self.reset.setShortcut("Ctrl+R")
+        options.addAction(self.reset)
+
+        self.exit = QAction(QIcon(resource_path("Images/exit.png")), "Exit")
+        options.addAction(self.exit)
